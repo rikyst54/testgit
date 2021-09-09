0a263d5c3287fcf8b9659b707428c46b669b72ab
Aissyla <alyssia.drissi@gmail.com>
2021-09-09 14:59:40 +0200

    add date

--- client.php
+++ client.php
@@ -4,6 +4,7 @@ class Client
 {
     private string $nom = "";
     private string $prenom = "";
+    private DateTime $date_naissance;
     private string $tel = "";
     private string $email = "";
     private string $adresse = "";
@@ -76,10 +77,13 @@ class Client
         return $this->age;
     }
 
-    public function __construct(string $nom, string $prenom, string $tel, string $email, string $adresse, string $ville, int $cp, int $age)
+    public function __construct(string $nom, string $prenom, int $jour, int $mois, int $annee, string $tel, string $email, string $adresse, string $ville, int $cp, int $age)
     {
         $this->nom = $nom;
         $this->prenom = $prenom;
+        $date_anniv = new DateTime();
+        $date_anniv->setDate($annee, $mois, $jour);
+        $this->date_naissance = $date_anniv;
         $this->tel = $tel;
         $this->email = $email;
         $this->adresse = $adresse;
@@ -87,20 +91,28 @@ class Client
         $this->cp = $cp;
         $this->age = $age;
     }
-    public function afficher()
+    public function setDateNaissance(int $annee, int $mois, int $jour)
     {
-        echo "Nom : " . $this->nom . "<br>";
-        echo "Prénom : " . $this->prenom . "<br>";
-        echo "Tèl. : " . $this->tel . "<br>";
-        echo "Email : " . $this->email . "<br>";
-        echo "Adresse : " . $this->adresse . "<br>";
-        echo "Ville : " . $this->ville . "<br>";
-        echo "Cp : " . $this->cp . "<br>";
-        echo "Age : " . $this->age;
+        $this->date_naissance->setDate($annee, $mois, $jour);
+    }
+    public function getDateNaissance()
+    {
+        return $this->date_naissance->format('d-m-Y');
+    }
+    function anniversaire()
+    {
+        $now = new DateTime('now');
+        // var_dump($this->date_naissance->format('d-m'));
+        if ($this->date_naissance->format('d-m') == ($now)->format('d-m')) {
+            return 'joyeux anniversaire ';
+        } else {
+            return  'c\'est pas ton anniversaire';
+        }
     }
 }
 
 // définir variable
-$client = new Client("test", "test", "00.00.00.00.00", "test@gmail.com", "test", "test", 00, 00);
+$client = new Client("test", "test", 9, 9, 2021, "00.00.00.00.00", "test@gmail.com", "test", "test", 00, 00);
 
-var_dump($client->afficher());
+echo $client->anniversaire();
+$aujo = new DateTime('now');
